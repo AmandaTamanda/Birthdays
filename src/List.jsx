ff9535e7204a8cb1e3165f43268615da19fe6d88
asefubeierl <asefubeierl@terrestris.de>
2025-12-05 12:53:10 +0100

    push project to git hub

---
+++ src/List.jsx
@@ -0,0 +1,14 @@
+import Person from "./Person";
+
+const List = ({ people }) => {
+  return (
+    <section>
+    {people.map((person) => {
+      console.log(person)
+      return <Person key={person.id} {...person} />;
+    })}
+    </section>
+  );
+};
+
+export default List;
